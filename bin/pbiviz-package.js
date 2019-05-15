f4512dc3a7a8443b5e2793ec710e911264fc8cd0
Amit Roy <amitroy.github@gmail.com>
2019-05-15 02:29:00 -0500

    commandline options descripton corrected

--- bin/pbiviz-package.js
+++ bin/pbiviz-package.js
@@ -39,7 +39,7 @@ program
     .option('--no-pbiviz', "Doesn't produce a pbiviz file (must be used in conjunction with resources flag)")
     .option('--no-minify', "Doesn't minify the js in the package (useful for debugging)")
     .option('--no-plugin', "Doesn't include a plugin declaration to the package (must be used in conjunction with --no-pbiviz and --resources flags)")
-    .option('--with-guid-prefix <prefix>', "Generates build with guid prefixed with 'VALQ_' to differentiate custom build from the published build")
+    .option('--with-guid-prefix <prefix>', "Generates build with guid prefixed with the given prefix to differentiate custom build from the published build")
     .option('--tag <tag>', "Adds provided tag to the visual's display name", '')
     .option('--build-config-files <files>', "Provide comma separated list of build json files")
     ;
